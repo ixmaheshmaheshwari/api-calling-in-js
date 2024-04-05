62e0186b68512d4d66501c026d0c534986fa78b0
ixmaheshmaheshwari <157010602+ixmaheshmaheshwari@users.noreply.github.com>
2024-04-05 14:40:54 +0530

    commit

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-const fetchData = async(url)=>{
-    const container= document.getElementById("data");
-   container.innerHTML = ""; 
+const fetchData = async (url) => {
+    const container = document.getElementById("data");
+    container.innerHTML = "";
     const loader = document.getElementById("loader");
     loader.style.display = "block";
-    const result= await fetch(url);
-  
-    const pokemonData = await result.json();
-    const img= pokemonData.results.map(async pokemon => await fetch(pokemon.url).then(response => response.json()))
-const imgData = await Promise.all(img);   
-imgData.forEach((img) => {
-   console.log(img.sprites.other.showdown.front_default)
-   });
- console.log(imgData)
-     totalCount=pokemonData.count;
-     pervURL=pokemonData.previous;
-     nextURL=pokemonData.next;
-    
-     setTimeout(() => {
-        loader.style.display = "none";
-        displayData(pokemonData.results,imgData);
-    }, 500)
+    const result = await fetch(url);
 
+    const pokemonData = await result.json();
+    const img = pokemonData.results.map(async pokemon => await fetch(pokemon.url).then(response => response.json()))
+    const imgData = await Promise.all(img);
+    imgData.forEach((img) => {
+        console.log(img.sprites.other.showdown.front_default)
+    });
+    console.log(imgData)
+    totalCount = pokemonData.count;
+    pervURL = pokemonData.previous;
+    nextURL = pokemonData.next;
+
+    setTimeout(async () => {
+        loader.style.display = "none";
+        await  displayData(pokemonData.results, imgData);
+    }, 1000)
+  
 
 }
-var totalCount=1302;
-var nextURL="";
-var pervURL="";
-const displayData = (pokemonList,imgData) => {
+var totalCount = 1302;
+var nextURL = "";
+var pervURL = "";
+const displayData = async (pokemonList, imgData) => {
     console.log(pokemonList)
 
-    const container= document.getElementById("data");
-   container.innerHTML = ""; 
-   const dataElement= document.createElement("div");
-   dataElement.id = "container"
-   let dataText=""
-   pokemonList.forEach((pokemon, index) => {
-    const imgUrl = imgData[index] ? imgData[index].sprites.other.showdown.front_default : "";
-    console.log(imgUrl)
+    const container = document.getElementById("data");
+    container.innerHTML = "";
+    const dataElement = document.createElement("div");
+    dataElement.id = "container"
+    let dataText = ""
+    pokemonList.forEach((pokemon, index) => {
+        const imgUrl = imgData[index] ? imgData[index].sprites.other.showdown.front_default : "";
+        console.log(imgUrl)
 
-        dataText+= `
+        dataText += `
         <div class="poke" style="background-image: url(${imgUrl})">${pokemon.name} </div>
        `;
-      
-        
-   });
-   dataElement.innerHTML=dataText
-   container.appendChild(dataElement);
-   console.log(pervURL,nextURL)
+
+
+    });
+    dataElement.innerHTML = dataText
+    container.appendChild(dataElement);
+    console.log(pervURL, nextURL)
 
     // console.log(pokemonData)
 
 };
-console.log(pervURL,nextURL)
+console.log(pervURL, nextURL)
 
 // const createPaginationControls = () => {
 //     $('#pagination-container').pagination({
@@ -73,29 +73,29 @@ console.log(pervURL,nextURL)
 
 const createPaginationControls = () => {
     $('#pagination-container').pagination({
-        dataSource: function(done) {
+        dataSource: function (done) {
             done(Array.from({ length: Math.ceil(totalCount / 10) }, (_, i) => i + 1));
         },
         pageSize: 1,
         showPrevious: true,
         showNext: true,
-      
+
         showPageNumbers: true,
-        callback: function(data, pagination) {
+        callback: function (data, pagination) {
             // We don't need to do anything here since we'll be using prevURL and nextURL to fetch data
         },
-        
 
-        afterPageOnClick: function(e, pageNumber) {
+
+        afterPageOnClick: function (e, pageNumber) {
             // This function is called after clicking on a page number
             const url = `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${(pageNumber - 1) * 10}`;
             fetchData(url);
         },
-        afterPreviousOnClick: function(e) {
+        afterPreviousOnClick: function (e) {
             // This function is called after clicking on the Previous button
             fetchData(prevURL);
         },
-        afterNextOnClick: function(e) {
+        afterNextOnClick: function (e) {
             // This function is called after clicking on the Next button
             fetchData(nextURL);
         }
@@ -103,22 +103,22 @@ const createPaginationControls = () => {
 };
 
 
-document.getElementById("fetchdata").addEventListener("click", function(){
-    const url="https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
+document.getElementById("fetchdata").addEventListener("click", function () {
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
     fetchData(url);
-    const pagination=document.querySelector("#pagination-container");
-    pagination.innerHTML="";
+    const pagination = document.querySelector("#pagination-container");
+    pagination.innerHTML = "";
     setTimeout(() => {
         createPaginationControls();
     }, 100)
-   
+
 
 })
-   
 
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    
+
+document.addEventListener("DOMContentLoaded", () => {
+
 
 })
 
